ddeee3bc2becc44cf045a2fb73639d94fc9817ff
carolina99goncalves <carolina.gonalves@hotmail.com>
2025-02-04 20:35:43 +0000

    FT01ex03

---
+++ UFCD10793_FT01/ex03.py
@@ -0,0 +1,8 @@
+'''um programa que receba, como parâmetro um inteiro, e devolva o se dobro.
+'''
+numero = input("introduza um valor inteiro:")
+numero = int(numero)
+
+dobro = 2*numero
+
+print("O dobro de", numero, "é", dobro) # Output: O dobro do
